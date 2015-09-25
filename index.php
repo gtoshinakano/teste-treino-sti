b34eaf38272761fa2f013f63337f4b2a59abe556
patyalves17 <patriciaalves10@gmail.com>
2015-09-25 14:46:39 -0300

    criado index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>DAEE - Portal</title>
+	</head>
+	<body>
+	<h1>Hello</h1>
+	<?php
+		echo "DATE 25/09/2015";
+	?>
+	</body>
+</html>
